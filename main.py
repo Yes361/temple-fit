c83745899a2a761b1a951ffb727f1884257fb144
Raiyan Islam <Raiyan.islam2@student.tdsb.on.ca>
2024-06-03 09:19:32 -0400

    Set up

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('hello raiyan islam')
+from pgzero.builtins import *
+import pgzrun
+from pgzhelper import *
+import os
+
+os.environ['SDL_VIDEO_CENTERED'] = '1' #Forces window to be centered on screen.
+WIDTH = 800
+HEIGHT = 600
+TITLE = ''
+
+def on_mouse_down(pos, button):
+    pass
+
+def on_key_down(key, unicode):
+    pass    
+
+def update(dt):
+    pass
+
+def draw():
+    pass
+
+pgzrun.go()
